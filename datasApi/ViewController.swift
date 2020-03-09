a0f8d5a111678a42b73f12fe1694b9516fd968aa
Bhargava_mike <saibhargava54@gmail.com>
2020-03-09 19:30:33 +0530

    deleted

--- datasApi/ViewController.swift
+++ datasApi/ViewController.swift
@@ -12,8 +12,7 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        print("challa bhargava  ")
-        print("image to data or from data entiran")
+        
     }
 
 
